86a1cda2d68723f54d946523a8badef0120a5cf1
Yegor Kazantsev <admin@dotsb.net.ru>
2015-04-01 12:31:55 +0600

    Update views.py
    Fix robots txt content - type

--- views.py
+++ views.py
@@ -8,4 +8,4 @@ def crossdomain(request):
     return render(request,'crossdomain.xml',{},content_type="application/xml")
 
 def robotstxt(request):
-    return render(request,'robots.txt',{},content_type="application/text")
+    return render(request,'robots.txt',{},content_type="text/plain")
